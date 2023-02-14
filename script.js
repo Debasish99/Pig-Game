8c57a56274a5cf1ee5d8bdfea44c675d75ce950e
Debasish Lenka <lenkadebasish977@gmail.com>
2023-02-14 17:28:22 +0530

    commit 1 🚀

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ btnHold.addEventListener('click',function() {
 
         //2. Check if player's score is >= 100
         
-        if(scores[activeplayer] >= 100){
+        if(scores[activeplayer] >= 50){
             //finish the game
             playing = false;
             document.querySelector(`.player--${activeplayer}`).classList.add('player--winner');
@@ -94,3 +94,35 @@ btnHold.addEventListener('click',function() {
 })
 
 btnNew.addEventListener('click',init);
+
+
+//show info modal
+
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal'); 
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function(){
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+
+const closeModal = function(){
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+for(let i =0; i <btnsOpenModal.length; i++){
+    btnsOpenModal[i].addEventListener('click',openModal);
+}
+
+btnCloseModal.addEventListener('click',closeModal)
+overlay.addEventListener('click',closeModal)
+
+document.addEventListener('keydown',function(e) {
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+        closeModal();
+    }
+})
